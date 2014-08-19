0e7ab660551c9bee738bb9c021746b105ff75964
Nicholas Woodland <nick@burfieldcreative.co.uk>
2014-08-19 15:01:11 +0100

    tiny little fix

--- includes/js/custom.js
+++ includes/js/custom.js
@@ -1,14 +1,14 @@
 jQuery( document ).ready( function( $ ) {
 
         var win_height      = $(window).height();
-        var banner_height   = $('.site-header').height();
+        var banner_height   = $('.site-header').outerHeight();
         var button_height   = $('.js-smooth-scroll').outerHeight();
         var hero_banner     = $('.above-the-fold');
         var target_height   = 0;
 
         // Force minimum height of the banner
         // Small viewports cause layout to break
-        target_height   = (win_height - banner_height < 400) ? '400' : win_height - (banner_height + button_height - 24 );
+        target_height   = (win_height - banner_height < 400) ? '400' : win_height - (banner_height + button_height );
 
         // Force banner height to be height of window minus height of site banner "header"
         hero_banner.css('height', target_height);
